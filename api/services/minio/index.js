f7cbb5a0ac79ed95ae05518e37786e329a33a8c4
Jatin Katyal <jatin.katyal13@gmail.com>
2020-12-25 03:40:01 +0530

    fix: minio service

--- api/services/minio/index.js
+++ api/services/minio/index.js
@@ -8,8 +8,6 @@ class MinioService {
       endPoint: config.MINIO.ENDPOINT,
       accessKey: config.MINIO.ACCESS_KEY,
       secretKey: config.MINIO.SECRET_KEY,
-      port: config.MINIO.PORT,
-      useSSL: config.APP.ENV === 'production',
     });
   }
 
