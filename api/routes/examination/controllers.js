ac5cfe26eba98553fe8ffd05b49e3493fc0d50f6
Jatin Katyal <jatin.katyal13@gmail.com>
2022-08-18 16:16:45 +0530

    temp

--- api/routes/examination/controllers.js
+++ api/routes/examination/controllers.js
@@ -1,74 +1,25 @@
+const DB = require('models');
 const BaseListController = require('base/controllers/listController');
 const BaseDetailController = require('base/controllers/detailController');
-const DB = require('models');
+const ExaminationSerializerOpts = require('serializer-opts/examination');
 
 class ExaminationListController extends BaseListController {
   model = DB.examinations;
-
-  generateWhereClause() {
-    const whereClause = super.generateWhereClause();
-
-    return {
-      ...whereClause,
-      unlisted: false,
-    };
-  }
+  modelName = DB.examinations.name;
+  serializerOpts = ExaminationSerializerOpts(DB.examinations.name);
 
   generateOrderClause() {
     return ['id'];
-  }
-}
-
-class RelationshipQuestionController extends BaseListController {
-  model = DB.questions;
-
-  generateWhereClause() {
-    return {
-      examinationId: this.request.params.id,
-    };
-  }
-
-  generateOrderClause() {
-    return ['id'];
-  }
-}
-
-class RelationshipPracticePaperController extends BaseListController {
-  model = DB.practicePapers;
-
-  generateWhereClause() {
-    return {
-      examinationId: this.request.params.id,
-    };
   }
 }
 
 class ExaminationDetailController extends BaseDetailController {
   model = DB.examinations;
-}
-
-class CurrentExamAttemptController extends BaseDetailController {
-  model = DB.examAttempts;
-  generateWhereClause() {
-    return {
-      examinationId: this.request.params.id,
-      userId: this.request.user.id,
-    };
-  }
-  getObject() {
-    return this.model.findOne({
-      where: {
-        ...this.generateWhereClause(),
-      },
-      include: this.generateIncludeClause(),
-    });
-  }
+  modelName = DB.examinations.name;
+  serializerOpts = ExaminationSerializerOpts(DB.examinations.name);
 }
 
 module.exports = {
   ExaminationListController,
-  RelationshipQuestionController,
   ExaminationDetailController,
-  CurrentExamAttemptController,
-  RelationshipPracticePaperController,
 };
