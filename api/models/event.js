20672ed9356e9b00efe33e9b00bd24f5d86a4bd3
Personal (Ishaan) <personal_ishaan@Ishaan.local>
2023-08-05 19:38:03 +0530

    add: events model

---
+++ api/models/event.js
@@ -0,0 +1,51 @@
+const JsonApiModel = require('base/jsonApiModel');
+const SerializerOpts = require('serializer-opts/order');
+
+class Event extends JsonApiModel {
+  static get serializerOpts() {
+    return SerializerOpts(this);
+  }
+
+  static associate() {}
+}
+
+module.exports = (sequelize, DataTypes) => {
+  Event.init(
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        defaultValue: '',
+        allowNull: false,
+      },
+      examinations: {
+        type: DataTypes.ARRAY(DataTypes.BIGINT),
+        allowNull: false,
+      },
+      startDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      endDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      primaryPrice: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+      },
+      secondaryPrice: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'events',
+    },
+  );
+  return Event;
+};
